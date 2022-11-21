f0a7b558c0b903bd3174fa85e29f3d86015fca1f
KIM-TABLE-NEXT <tkdduq118@Naver.com>
2022-11-21 13:42:41 +0900

    add rankview

---
+++ app/src/main/java/com/example/imagecup/ui/adapter/RankAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.imagecup.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.imagecup.databinding.ItemGalleryBinding
+
+
+class RankAdapter{
+}
