a6b34174d5c403ea43adb794190585bcd31d1372
KIM-TABLE-NEXT <tkdduq118@Naver.com>
2022-12-08 15:31:08 +0900

    별점 클릭 이벤트 구현

--- app/src/main/java/com/example/imagecup/ui/RatingFragment.kt
+++ app/src/main/java/com/example/imagecup/ui/RatingFragment.kt
@@ -3,6 +3,7 @@ package com.example.imagecup.ui
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
@@ -25,12 +26,65 @@ class RatingFragment : BaseFragment<FragmentRatingBinding>(R.layout.fragment_rat
     private val viewModel: MainViewModel by viewModels()
 
     override fun createView(binding: FragmentRatingBinding) {
-
+        setListener()
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun viewCreated() {
         binding.tvLabel.text = arguments?.getString("label").toString()
     }
+
+    private fun setListener() {
+        binding.ivStar6.setOnClickListener {
+            binding.ivStar6.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar7.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar8.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar9.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar10.setImageResource(R.drawable.ic_emptystar)
+            Toast.makeText(requireContext(), "1점", Toast.LENGTH_LONG).show()
+            }
+        binding.ivStar7.setOnClickListener {
+            binding.ivStar6.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar7.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar8.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar9.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar10.setImageResource(R.drawable.ic_emptystar)
+            Toast.makeText(requireContext(), "2점", Toast.LENGTH_SHORT).show()
+        }
+        binding.ivStar8.setOnClickListener {
+            binding.ivStar6.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar7.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar8.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar9.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar10.setImageResource(R.drawable.ic_emptystar)
+            Toast.makeText(requireContext(), "3점", Toast.LENGTH_SHORT).show()
+        }
+        binding.ivStar9.setOnClickListener {
+            binding.ivStar6.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar7.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar8.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar9.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar10.setImageResource(R.drawable.ic_emptystar)
+            Toast.makeText(requireContext(), "4점", Toast.LENGTH_SHORT).show()
+        }
+        binding.ivStar10.setOnClickListener {
+            binding.ivStar6.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar7.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar8.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar9.setImageResource(R.drawable.ic_fullstar)
+            binding.ivStar10.setImageResource(R.drawable.ic_fullstar)
+            Toast.makeText(requireContext(), "5점", Toast.LENGTH_SHORT).show()
+        }
+        binding.tvRate.setOnClickListener {
+            binding.ivStar6.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar7.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar8.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar9.setImageResource(R.drawable.ic_emptystar)
+            binding.ivStar10.setImageResource(R.drawable.ic_emptystar)
+            Toast.makeText(requireContext(), "평가완료", Toast.LENGTH_SHORT).show()
+
+        }
+        }
 }
